05ede9ed9b747f4fc7e83ab753609cadc09397e8
Eduardo Coelho <eduardohrcoelhomkt@gmail.com>
2025-08-28 21:18:20 -0300

    Aplica conceito inicial de polimorfismo // Applies the initial concept of polymorphism

--- src/exercise01_business_account/application/Program.java
+++ src/exercise01_business_account/application/Program.java
@@ -17,5 +17,14 @@ public class Program {
         System.out.println( acc2.getBalance());
         acc3.withdraw(200.0);
         System.out.println(acc3.getBalance());
+
+        Account x = new Account(1020, "Josh", 1000.0);
+        Account y = new SavingsAccount(1021, "Mike", 1000.0, 0.01);
+
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
     }
 }
